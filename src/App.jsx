a3298da15457624513c956fb487051cc25c0b667
Zephyr-A0 <abdulkid151@gmail.com>
2025-12-07 11:53:37 +0100

    feat: update background images for responsive design and enhance text positioning in App component

--- src/App.jsx
+++ src/App.jsx
@@ -15,14 +15,15 @@ function App() {
       <Brands />
       <Collections />
       <div
-        className="relative lg:bg-cover h-[80vh] font-inter text-white"
-        style={{ backgroundImage: `url(${watchBg})` }}
+        className="relative bg-[url(./assets/watch2-bg-mobile.png)] md:bg-[url(./assets/watch2-bg.png)] bg-cover bg-center h-[80vh] font-inter text-white"
+        // style={{ backgroundImage: `url(${watchBg})` }}
       >
-        <p className="absolute right-0 pt-20 pr-24 max-w-[30%]">
+        <p className="absolute right-0 pt-6 md:pt-20 pr-6 md:pr-24 max-w-[70%] md:max-w-[30%]">
           Own a piece of history with our limited-edition watches, crafted in
           small quantities and featuring rare materials.
         </p>
-        <div className="absolute flex flex-col bottom-0 pl-24 pb-30">
+
+        <div className="absolute flex flex-col bottom-0 pl-6 md:pl-24 pb-6 md:pb-30">
           <p className="text-5xl">Limited Editions</p>
           <p className="max-w-[70%]">
             Discover the charm of vintage watches or invest in pre-owned
